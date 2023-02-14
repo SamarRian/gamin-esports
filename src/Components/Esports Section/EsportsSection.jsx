550c02c863a2394a2f09efe491dbe30f68544c32
SamarRian <samarrian4@gmail.com>
2023-02-14 17:40:39 +0500

    LATEST CODE

--- src/Components/Esports Section/EsportsSection.jsx
+++ src/Components/Esports Section/EsportsSection.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Esports from '../4AG Esports/Esports'
 import Esports2 from '../4AG Esports2/Esports2'
+import Services from '../Services/Services'
 import './EsportsSection.scss'
 const EsportsSection = () => {
   return (
     <div>
       <Esports />
       <Esports2 />
+      <Services />
     </div>
   )
 }
---
+++ src/Components/Services/Services.jsx
@@ -0,0 +1,72 @@
+import { Component } from "react";
+import "./Services.scss";
+import "./Services2.scss";
+import { AiFillThunderbolt,AiTwotoneStar } from "react-icons/ai";
+import { BsShieldFillCheck } from "react-icons/bs";
+import {FaThumbsUp } from "react-icons/fa";
+class Services extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div className="Services-Container">
+        <div className="Services-Container__item-1">
+         
+          <div className="Services-Container__Icon-div">
+            <AiFillThunderbolt className="Services-Container--Icon" />
+          </div>
+        
+          <p className="Services-Container__Heading">Tournament Esports</p>
+          <p className="Services-Container__Para">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+            doloremque harum soluta quidem temporibus voluptatem 
+          </p>
+          
+        </div>
+        <div className="Services-Container__item-2">
+         
+          <div className="Services-Container__Icon-div-2">
+          <BsShieldFillCheck className="Services-Container--Icon-2"/>
+          </div>
+        
+          <p className="Services-Container__Heading-2">Services</p>
+          <p className="Services-Container__Para-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+            doloremque harum soluta quidem temporibus voluptatem 
+          </p>
+          
+        </div>
+
+        <div className="Services-Container__item-3">
+         
+          <div className="Services-Container__Icon-div-3">
+          <FaThumbsUp className="Services-Container--Icon-3"/>
+          </div>
+        
+          <p className="Services-Container__Heading-3">Prize Pool</p>
+          <p className="Services-Container__Para-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+            doloremque harum soluta quidem temporibus voluptatem 
+          </p>
+        </div>
+
+        <div className="Services-Container__item-4">
+         
+          <div className="Services-Container__Icon-div-4">
+          <AiTwotoneStar className="Services-Container--Icon-4"/>
+          </div>
+        
+          <p className="Services-Container__Heading-4">Events</p>
+          <p className="Services-Container__Para-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+            doloremque harum soluta quidem temporibus voluptatem 
+          </p>
+        </div>
+          
+
+      </div>
+    );
+  }
+}
+export default Services;
